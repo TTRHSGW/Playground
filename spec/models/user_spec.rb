d803ef0754ce97d7e6e68a7b63dcca24910f8127
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-01 23:49:14 +0900

    Add spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "有効なモデルを持つこと" do
+    user = User.new(
+      username: 'test_user',
+      password: 'password'
+    )
+    expect(user).to be_valid
+  end
+
+  it "usernameが6文字未満だと無効な状態であること" do
+    user = User.new(
+      username: 'test',
+      password: 'password'
+    )
+    user.valid?
+    expect(user.errors[:username]).to include("is too short (minimum is 6 characters)")
+  end
+
 end
