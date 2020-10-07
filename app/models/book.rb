82d1e59b4863083774a5d10d9aaefab8442a5755
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-07 22:04:49 +0900

    Add association to user and book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   validates :date_of_publication, presence: true
   validates :title, presence: true
+  has_many :users, through: :user_books
+  has_many :user_books
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 6 }
   validates :password, presence: true, length: { minimum: 6 }
+  has_many :books, through: :user_books
+  has_many :user_books
   has_secure_password
 end
