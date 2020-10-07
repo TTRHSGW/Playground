b9a74377e64a87be268b4c110c324176a346edd4
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-07 21:58:53 +0900

    Add user_book table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_133721) do
+ActiveRecord::Schema.define(version: 2020_10_07_124956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_10_05_133721) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_books", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_user_books_on_book_id"
+    t.index ["user_id"], name: "index_user_books_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -29,4 +38,6 @@ ActiveRecord::Schema.define(version: 2020_10_05_133721) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_books", "books"
+  add_foreign_key "user_books", "users"
 end
