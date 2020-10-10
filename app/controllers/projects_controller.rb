ee4b04da58b394b5ba2f87db29d1dd3ee1a903aa
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-10 20:50:14 +0900

    Add method to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,35 @@
 class ProjectsController < ApplicationController
-  def index
-  end
+  before_action :set_project, only: %i[show edit update destroy]
 
-  def show
-  end
+  def index;end
+
+  def show;end
 
   def new
+    @project = Project.new
   end
 
-  def edit
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project
+    else
+      render :new
+    end
   end
+
+  def edit;end
+
+  def update;end
+
+    private
+
+    def project_params
+      params.require(:project).permit(:title)
+    end
+
+    def set_project
+      @project = Project(params[:id])
+    end
+
 end
--- spec/requests/projects_request_spec.rb
+++ spec/requests/projects_request_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe "Projects", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
-      get "/projects/index"
+      get projects_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/projects/show"
+      project = FactoryBot.create(:project)
+      get projects_path(project)
       expect(response).to have_http_status(:success)
     end
   end
@@ -19,15 +20,30 @@ RSpec.describe "Projects", type: :request do
   describe "GET /new" do
     it "returns http success" do
       get "/projects/new"
+      expect(response.body).to include('Projects#new')
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/projects/edit"
-      expect(response).to have_http_status(:success)
+  describe 'POST /users' do
+    context 'titleが有効な時' do
+      it 'Projectが登録されること' do
+        expect do
+          post projects_path, params: { project: { title: 'プロジェクト'} }
+          expect(response).to have_http_status(302)
+        end.to change(Project, :count).by(1)
+      end
+    end
+
+    context 'titleが無効な時' do
+      it 'Projectが登録されないこと' do
+        expect do
+          post projects_path, params: { project: { title: nil } }
+          expect(response.body).to include("can&#39;t be blank")
+        end.to change(Project, :count).by(0)
+      end
     end
   end
+
 
 end
