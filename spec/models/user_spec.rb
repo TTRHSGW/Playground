bd6b24dba9817c01b8382e9fdeac1c947523e8fe
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-16 22:49:03 +0900

    Add follow unfollow method and model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'フォローができること' do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.create(:user)
+    expect(user1.following?(user2)).to eq false
+    expect(user2.followers.include?(user1)).to eq false
+    user1.follow(user2)
+    expect(user1.following?(user2)).to eq true
+    expect(user2.followers.include?(user1)).to eq true
+  end
+
+  it 'アンフォローができること' do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.create(:user)
+    user1.follow(user2)
+    expect(user1.following?(user2)).to eq true
+    expect(user2.followers.include?(user1)).to eq true
+    user1.unfollow(user2)
+    expect(user1.following?(user2)).to eq false
+    expect(user2.followers.include?(user1)).to eq false
+  end
+
 end
