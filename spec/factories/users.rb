bfeb3f0f86bd4439eaedc6626acd2f2eee0ab791
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-16 11:58:52 +0900

    Add model spec for relationship

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "example#{n}@example.com" }
+    sequence(:password) { |n| "password#{n}" }
+    sequence(:username) { |n| "Username#{n}" }
   end
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Factory test' do
+    context 'ユーザーが存在する時' do
+      it 'Relationshipは登録できること' do
+        user1 = FactoryBot.create(:user)
+        user2 = FactoryBot.create(:user)
+        relationship = FactoryBot.create(:relationship, follower_id: user1.id, followed_id: user2.id)
+        expect(relationship).to be_valid
+      end
+    end
+  end
+
 end
