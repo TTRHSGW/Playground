69946d0f0ca92f50aaab0af5984b9fa60bf718a0
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-17 23:55:09 +0900

    Add follow button

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,8 @@
 class RelationshipsController < ApplicationController
   before_action :check_signed_in
   def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
   end
 
   def destroy
