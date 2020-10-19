f2d720704e95bb26818b9c7b481aec4464405939
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-19 09:07:36 +0900

    Add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :check_signed_in
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
   devise_for :users
-  resources :users, only: %i[show]
+  resources :users, only: %i[show index]
   get '/following' => 'users#following'
   get '/followers' => 'users#followers'
   get 'pages/show'
