fd7fe54e757fc459cce3f804eb29bffd8bf5a840
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-21 08:22:50 +0900

    Add relationship validates test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "example#{n}@example.com" }
+    sequence(:password) { |n| "password#{n}" }
   end
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates test' do
+    let(:user1){ FactoryBot.create(:user) }
+    let(:user2){ FactoryBot.create(:user) }
+    context 'followed_idがない時' do
+      it '無効な状態であること' do
+        relationship = FactoryBot.build(:relationship, follower_id: user1.id, followed_id: nil)
+        expect(relationship).to_not be_valid
+      end
+    end
+    context 'follower_idがない時' do
+      it '無効な状態であること' do
+        relationship = FactoryBot.build(:relationship, follower_id: nil, followed_id: user2.id )
+        expect(relationship).to_not be_valid
+      end
+    end
+    context 'followed_id, follower_idがある時' do
+      it '有効な状態であること' do
+        relationship = FactoryBot.build(:relationship, follower_id: user1.id, followed_id: user2.id )
+        expect(relationship).to be_valid
+      end
+    end
+  end
 end
