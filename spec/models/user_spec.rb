5713affb7120c8479fc0aea012fb254069ec86ff
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-21 11:12:49 +0900

    Add follow unfollow method test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Factory Test' do
+    it '有効なFactoryであること' do
+      user = FactoryBot.create(:user)
+      expect(user).to be_valid
+    end
+  end
+
+  describe 'method test' do
+    let(:user1){ FactoryBot.create(:user) }
+    let(:user2){ FactoryBot.create(:user) }
+    it 'フォロー、アンフォローができること' do
+      expect(user1.following?(user2)).to eq false
+      user1.follow(user2)
+      expect(user1.following?(user2)).to eq true
+      user1.unfollow(user2)
+      expect(user1.following?(user2)).to eq false
+    end
+  end
 end
