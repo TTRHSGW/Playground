a056909f76441918c2b08595eb04cee4c68b2289
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-21 11:44:19 +0900

    Add top and user index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: %i[show index]
   resources :tweets, only: %i[create destroy]
   root 'pages#home'
   get 'pages/mypage'
