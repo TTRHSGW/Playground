3e8f5455b9154e69d8e51bd4ffe8503689b438f6
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-21 11:31:39 +0900

    Add Tweet model controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :tweets, only: %i[create destroy]
   root 'pages#home'
   get 'pages/mypage'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_142903) do
+ActiveRecord::Schema.define(version: 2020_10_21_021944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2020_10_20_142903) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_10_20_142903) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
