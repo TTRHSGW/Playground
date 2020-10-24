c52ccd07c0f82efb4b3fe68852849673096ca58b
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-24 09:25:52 +0900

    Add request spec for relationship unfollow

--- spec/requests/relationships_request_spec.rb
+++ spec/requests/relationships_request_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Relationships", type: :request do
-  describe 'フォローした時' do
-    it 'following.countが増えること' do
-      user1 = FactoryBot.create(:user)
-      user2 = FactoryBot.create(:user)
-      sign_in user1
-      expect {
-        post relationships_path, params: { followed_id: user2.id }, xhr: true
-      }.to change(Relationship, :count).by(1)
+  describe 'フォローアンフォローテスト' do
+    context 'フォローした時' do
+      it 'following.countが増えること' do
+        user1 = FactoryBot.create(:user)
+        user2 = FactoryBot.create(:user)
+        sign_in user1
+        expect {
+          post relationships_path, params: { followed_id: user2.id }, xhr: true
+        }.to change(Relationship, :count).by(1)
+      end
+    end
+    context 'フォロー解除した時' do
+      it 'following.countが減ること' do
+        user1 = FactoryBot.create(:user)
+        user2 = FactoryBot.create(:user)
+        user1.follow(user2)
+        relationship = user1.active_relationships.find_by(followed_id: user2.id)
+        sign_in user1
+        expect {
+          delete relationship_path(relationship), xhr: true
+        }.to change(Relationship, :count).by(-1)
+      end
     end
   end
 end
