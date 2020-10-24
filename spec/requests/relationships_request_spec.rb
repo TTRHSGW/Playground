bd042d15ff4e0954e64623d3579b26911aa00679
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-24 09:15:22 +0900

    Add request spec for relationship

--- spec/requests/relationships_request_spec.rb
+++ spec/requests/relationships_request_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Relationships", type: :request do
-
+  describe 'フォローした時' do
+    it 'following.countが増えること' do
+      user1 = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user)
+      sign_in user1
+      expect {
+        post relationships_path, params: { followed_id: user2.id }, xhr: true
+      }.to change(Relationship, :count).by(1)
+    end
+  end
 end
