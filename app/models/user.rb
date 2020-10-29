daf1417dbfb61dd8b5a5f0681cc04e8a054452c8
TTRHSGW <thisisnotajob2015@gmail.com>
2020-10-29 09:29:25 +0900

    Fix feed method with includes and Fix count to size for N + 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ApplicationRecord
 
   def feed
     following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
-    Tweet.where("user_id IN (#{following_ids}) OR user_id = :user_id ", user_id: id).order("created_at DESC")
+    Tweet.includes(:user, :likes).where("user_id IN (#{following_ids}) OR user_id = :user_id ", user_id: id).order("created_at DESC")
   end
 
 end
